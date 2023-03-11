bdd408af9fc3e84e2e2bb40307d16414ceb0df16
Lewis <lewispickersgill123@gmail.com>
2023-03-11 17:13:04 +0000

    Added a specific timer function, changed MessageBox.Show into a PopUp method and updated ReadME.

--- Clock Project/Program.cs
+++ Clock Project/Program.cs
@@ -16,6 +16,20 @@ namespace Clock_Project
             DateTime time = DateTime.Now;
             return "The current time is: " + time.ToShortTimeString();
             }
+        public static void SpecificTimer(DateTime reminderTime)
+        {
+            Console.Clear();
+            Console.SetWindowSize(10, 10);
+            while (reminderTime >= DateTime.Now)
+            {
+                Thread.Sleep(15 * 1000);
+            }
+            PopUp();
+        }
+        public static void PopUp()
+        {
+            MessageBox.Show(CurrentTime(), "Time", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+        }
     }
 
 
@@ -23,18 +37,23 @@ namespace Clock_Project
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize(100, 10);
-
             Console.WriteLine("Welcome to my clock application");
-            Console.WriteLine("Would you like to be reminded the time periodically? Please answer yes or no: ");
+            Console.WriteLine("Please select from one of the following options:\n1. Set a reminder for a specific time\n2. Set a frequent reminder");
+            Console.WriteLine("Please enter the number of the option you would like to select: ");
             string decision = Console.ReadLine();
             bool closeApp = false;
 
-            if (decision == "yes" || decision == "Yes")
+            if (decision == "1")
+            {
+                Console.WriteLine("What time would you like to be reminded? Please respond in the 24 hour format, e.g 17:34");
+                DateTime endTime = Convert.ToDateTime(Console.ReadLine());
+                Time.SpecificTimer(endTime);
+            }
+
+            if (decision == "2") 
             {
                 Console.WriteLine("Okay, how often would you like to be reminded? Please enter below in minutes: ");
                 int frequency = Convert.ToInt32(Console.ReadLine());
-
                 DateTime startTime = DateTime.Now;
                 DateTime endTime = startTime.AddMinutes(frequency);    
 
@@ -47,7 +66,7 @@ namespace Clock_Project
 
                     if (DateTime.Now >= endTime)
                     {
-                        MessageBox.Show(Time.CurrentTime());
+                        Time.PopUp();
                         startTime = DateTime.Now;
                         endTime = startTime.AddMinutes(frequency);  
                     }
